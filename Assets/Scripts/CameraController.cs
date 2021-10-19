e0ba0933cf12d0000eb4376362bcbdbf9320a424
Orzoch <47740876+kziel445@users.noreply.github.com>
2021-10-19 23:40:46 +0200

    camera 2 options

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+    public bool useOffSetValues;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {   
+        if (useOffSetValues) offset = new Vector3(5, -5, 5);
+        else offset = player.position - transform.position;
+        transform.position = player.position - offset;
+        transform.LookAt(player.transform);
+    }
+}
