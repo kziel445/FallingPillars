6f2f90ea049a786670fd757a828c47303cfcbad9
Orzoch <47740876+kziel445@users.noreply.github.com>
2021-10-19 19:41:11 +0200

    variables for moving script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
+    public float moveSpeed;
+    public Rigidbody rb;
     void Start()
     {
         
