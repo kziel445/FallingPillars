34b3b89e803ac1c2958ca4e4700a327c3f576c62
Kamil Zieliński <47740876+kziel445@users.noreply.github.com>
2021-10-19 19:57:21 +0200

    Moving (#2)
    * create hierarchy
    
    * script for player movement
    
    * variables for moving script
    
    * set up moving script
    
    * add jump to script
    
    * some colors
    
    * Update scene

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+    public Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float xAxis = Input.GetAxis("Horizontal");
+        float zAxis = Input.GetAxis("Vertical");
+        rb.velocity = new Vector3(xAxis * moveSpeed, rb.velocity.y, zAxis * moveSpeed);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+        }
+    }
+}
