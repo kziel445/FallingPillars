e3a15f28b530dba9ff64055611f7208151973dfe
Orzoch <47740876+kziel445@users.noreply.github.com>
2021-10-20 17:18:03 +0200

    improve rotation y

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -9,9 +9,15 @@ public class CameraController : MonoBehaviour
     public Vector3 offset;
     public bool useOffSetValues = true;
     public float rotateSpeed;
+    public Transform pivot;
+
     void Start()
     {
         if (!useOffSetValues) offset = player.position - transform.position;
+        pivot.transform.position = player.transform.position;
+        pivot.transform.parent = player.transform;
+
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
@@ -20,10 +26,10 @@ public class CameraController : MonoBehaviour
         player.Rotate(0, horizontal, 0);
 
         float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
-        player.Rotate(-vertical, 0, 0);
+        pivot.Rotate(-vertical, 0, 0);
 
         float desiredYAngle = player.eulerAngles.y;
-        float desiredXAngle = player.eulerAngles.x;
+        float desiredXAngle = pivot.eulerAngles.x;
         Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
 
         transform.position = player.position - (rotation * offset);
