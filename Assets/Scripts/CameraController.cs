318be0275f4af96b236eafd5ce0fe832965dba27
Kamil Zieliński <47740876+kziel445@users.noreply.github.com>
2021-10-20 18:28:33 +0200

    Moving with camera (#7)
    * improve rotation y
    
    * moving during jump
    
    moving during jump with added 1/4 of max speed

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -9,25 +9,32 @@ public class CameraController : MonoBehaviour
     public Vector3 offset;
     public bool useOffSetValues = true;
     public float rotateSpeed;
+    public Transform pivot;
+
     void Start()
     {
         if (!useOffSetValues) offset = player.position - transform.position;
+        pivot.transform.position = player.transform.position;
+        pivot.transform.parent = player.transform;
+
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    void Update()
+    void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
         player.Rotate(0, horizontal, 0);
 
         float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
-        player.Rotate(-vertical, 0, 0);
+        pivot.Rotate(-vertical, 0, 0);
 
         float desiredYAngle = player.eulerAngles.y;
-        float desiredXAngle = player.eulerAngles.x;
+        float desiredXAngle = pivot.eulerAngles.x;
         Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
 
         transform.position = player.position - (rotation * offset);
-        //transform.position = player.position - offset;
+
+        if (transform.position.y < player.position.y) transform.position = new Vector3(transform.position.x, player.position.y - 0.5f, transform.position.z);
         transform.LookAt(player.transform);
 
         //if(Input.GetKeyDown("c")) useOffSetValues = !useOffSetValues;
