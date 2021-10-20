8b4859c6873335d8808959591450f18bdac61891
Kamil Zieliński <47740876+kziel445@users.noreply.github.com>
2021-10-20 16:56:03 +0200

    Camera rotation (#6)
    * horizontal and vertical camera rotation
    
    * Update SampleScene.unity

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,20 +5,33 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Transform player;
+    public Camera camera;
     public Vector3 offset;
     public bool useOffSetValues = true;
+    public float rotateSpeed;
     void Start()
     {
-        
+        if (!useOffSetValues) offset = player.position - transform.position;
     }
 
     void Update()
-    {   
-        if(Input.GetKeyDown("c")) useOffSetValues = !useOffSetValues;
+    {
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        player.Rotate(0, horizontal, 0);
 
-        if (useOffSetValues) offset = new Vector3(5, -5, 5);
-        else offset = player.position - transform.position;
-        transform.position = player.position - offset;
+        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+        player.Rotate(-vertical, 0, 0);
+
+        float desiredYAngle = player.eulerAngles.y;
+        float desiredXAngle = player.eulerAngles.x;
+        Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
+
+        transform.position = player.position - (rotation * offset);
+        //transform.position = player.position - offset;
         transform.LookAt(player.transform);
+
+        //if(Input.GetKeyDown("c")) useOffSetValues = !useOffSetValues;
+        //if (!useOffSetValues) offset = player.position - transform.position;
+
     }
 }
